9d95d43f0924e077c9b3b05b7543d2afff6368fa
100yenlabo <79507421+100yenlabo@users.noreply.github.com>
2025-02-03 21:01:20 +0900

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// ページ内スクロール機能
-document.querySelector('.cta-btn').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('#services').scrollIntoView({ behavior: 'smooth' });
-});
